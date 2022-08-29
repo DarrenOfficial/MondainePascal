4c302dff6416be4404b0eece2f1a965893ed48a9
Darren Nathanael <git@darrennathanael.com>
2022-08-29 14:22:23 +0700

    Amazing Code

--- master.pas
+++ master.pas
@@ -1,6 +1,13 @@
 program hello_world;
 begin
+    TextColor(14);
     writeln('Hello world!');
-    writeln('I am learning pascal! this is the first time');
+    writeln('I am learning pascal! this is the first line of the hello world');
+    writeln('This is some text for the second line of the hello world');
+    writeln('This is some text for the third line of the hello world');
+    writeln('This is some text for the fourth line of the hello world');
+    writeln('This is some text for the fifth line of the hello world');
+    writeln('This is some text for the sixth line of the hello world');
+    writeln('This is some text for the seventh line of the hello world');
     readln;
 end.
