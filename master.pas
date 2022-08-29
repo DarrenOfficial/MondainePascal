d53d765b3b821a020962f2b9b76d404b15e6a97a
Darren Nathanael <git@darrennathanael.com>
2022-08-29 14:28:17 +0700

    eeeeeee

--- master.pas
+++ master.pas
@@ -1,6 +1,5 @@
 program hello_world;
 begin
-    TextColor(14);
     writeln('Hello world!');
     writeln('I am learning pascal! this is the first line of the hello world');
     writeln('This is some text for the second line of the hello world');
@@ -9,5 +8,6 @@ begin
     writeln('This is some text for the fifth line of the hello world');
     writeln('This is some text for the sixth line of the hello world');
     writeln('This is some text for the seventh line of the hello world');
+    writeln('This is some text for the eighth line of the hello world');
     readln;
 end.
